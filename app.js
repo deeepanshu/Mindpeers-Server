9bac846aac360e14b8b46223030576702edc14fc
Deepanshu Jain <deepanshujain1234@live.com>
2019-06-05 23:42:25 +0530

    Mongoose Setup

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('./services/express');
-const morgan  = require('morgan');
 const credentials = require('./config/credentials');
 const routes = require('./api');
 const http = require('http');
-
+const mongoose = require('./services/mongoose');
 const app = express(credentials.apiRoot, routes);
+
+
+mongoose.connect(credentials.mongoURI, { useNewUrlParser: true })
 
 const server = http.createServer(app);
 
 setImmediate(() => {
-    server.listen(credentials.PORT, credentials.ip, () => {
-        console.log('Express server listening on http://%s:%d', credentials.ip, credentials.PORT);
+    server.listen(credentials.port, credentials.ip, () => {
+        console.log('Express server listening on http://%s:%d', credentials.ip, credentials.port);
     })
 })
